7dc37cac333501c693d59f01cd875e54d3a0d431
BamideleHenry202 <bamidelehenry299@gmail.com>
2025-01-07 10:14:12 +0100

    page5

--- page5.js
+++ page5.js
@@ -59,12 +59,20 @@ document.querySelectorAll(".image-slider").forEach((slider) => {
             guideAddress:" Delight Hotels Ado Ekiti is 1, Ado - Ilawe Road, Ado-Ekiti,Ekiti State, Nigeria",
             guideDescription:"Delight Hotels offers a blend of modern comfort and exceptional hospitality. This elegant hotel provides a relaxing retreat for both business and leisure travelers. Guests can enjoy well-furnished rooms, a variety of amenities, and personalized services designed to meet the needs of every visitor"
           },
-          {
-            image: "images/chickenRepublic.jpg",
+         {
+    
+                  image: "images/chickenRepublic.jpg",
             guideName: "Chicken Republic",
             guideAddress: " Sectariat,New-Iyin road, Ado Ekiti,360211, Ado-Ekiti Ekiti state",
             guideDescription:" Chicken Republic in Ado Ekiti is a popular fast-food chain that offers a delicious selection of affordable and mouthwatering meals. Known for its signature crispy fried chicken, Chicken Republic serves a variety of tasty dishes, including hearty meals, wraps, burgers, and flavorful sides. With a focus on quality and freshness, the restaurant provides a comfortable and casual dining experience for families, friends, and busy professionals alike"
           }, 
+          
+    {
+          image: "images/amalaSpot.jpg",
+          guideName: "Amala Spot",
+          guideAddress: "Amala Spot is located at 5, NTA-Ilawe Bypass (after the Federal Polytechnic Ado-Ekiti satellite campus) ",
+          guideDescription:" Amala Spot in Ado Ekiti is a popular local eatery known for serving some of the best traditional Nigerian dishes, with a special focus on Amala – a beloved Yoruba delicacy made from yam flour. The restaurant offers a cozy, no-frills environment where guests can enjoy delicious, home-cooked meals in a relaxed setting. At Amala Spot, the star attraction is, of course, the Amala served with a variety of rich and flavorful soups, such as Efo Riro, Gbegiri, and Ogbono, along with tender meats like Goat meat, Beef, and Assorted meats."
+        }, 
           
   ];
   
